925ab2e2719f57a7810751172d57be109ec8d13d
yaluvv <yaluvv@yandex.ru>
2022-08-17 13:51:15 +0300

    added burger menu for header nav, added anchor links

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,10 @@ const sectionBtn = document.querySelector('.hero__btn');
 const popup = document.querySelector('.popup');
 const closePopupBtn = document.querySelector('.popup__close');
 const body = document.body;
+const headerNavLinks = document.querySelectorAll('.header__nav-link');
+const burgerMenu = document.querySelector('.burger-menu')
+const headerNav = document.querySelector('.header__nav')
+let lengthLinks = headerNavLinks.length
 
 sectionBtn.addEventListener('click', () => {
     popup.classList.toggle('popup--active')
@@ -19,3 +23,41 @@ popup.addEventListener('click', (e) => {
         body.classList.remove('scroll-hidden')
     }
 })
+const makeScrollHidden = () => {
+    if (headerNav.classList.contains('header__nav--active')) {
+        body.classList.add('scroll-hidden')
+    } else {
+        body.classList.remove('scroll-hidden')
+    }
+}
+
+burgerMenu.addEventListener('click', () => {
+
+    headerNav.classList.toggle('header__nav--active')
+    burgerMenu.classList.toggle('burger-menu--active')
+
+    makeScrollHidden()
+
+})
+
+headerNavLinks.forEach(link => link.addEventListener('click', () => {
+    headerNav.classList.remove('header__nav--active')
+    burgerMenu.classList.remove('burger-menu--active')
+    makeScrollHidden()
+}
+))
+
+function navActive(el) {
+    for (let i = 0; i < lengthLinks; i++) {
+        headerNavLinks[i].classList.remove("header__nav-link--active")
+    }
+
+    el.classList.add("header__nav-link--active")
+}
+
+for (let i = 0; i < lengthLinks; i++) {
+    headerNavLinks[i].addEventListener("click", function () {
+        navActive(this);
+    })
+}
+
